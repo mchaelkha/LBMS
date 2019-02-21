ab43ce274935ee8efe9f827b126ec129ee55560b
Michael Kha <mxk5025@rit.edu>
2019-02-21 17:48:11 -0500

    Documented visitor classes

--- src/Visitor/VisitorDB.java
+++ src/Visitor/VisitorDB.java
@@ -8,27 +8,51 @@ import java.util.Map;
  */
 public class VisitorDB {
 
+    /**
+     * All visitors that have been logged
+     */
     private Map<String, VisitorInfo> visitors;
-
+    /**
+     * All current visitors in the library
+     */
     private Map<String, VisitorInfo> currentVisitors;
 
+    /**
+     * Create a new visitor database that is empty.
+     */
     public VisitorDB() {
         visitors = new HashMap<>();
         currentVisitors = new HashMap<>();
     }
 
+    /**
+     * Register the visitor given properly formatted info.
+     * The new visitor is added into the map of visitors.
+     * @param info The info needed to create a visitor
+     */
     public void registerVisitor(String info) {
-
+        //might return visitorinfo
     }
 
+    /**
+     * Start a visit given the visitor id.
+     * @param visitorID The visitor id to log a visit with
+     */
     public void startVisit(String visitorID) {
 
     }
 
+    /**
+     * End a visit that is in progress given the visitor id.
+     * @param visitorID The visitor id to end a visit for
+     */
     public void endVisit(String visitorID) {
 
     }
 
+    /**
+     * Clear all the current visitors that are logged.
+     */
     public void clearCurrentVisitors() {
 
     }
--- src/Visitor/VisitorInfo.java
+++ src/Visitor/VisitorInfo.java
@@ -7,36 +7,72 @@ import java.util.Map;
  */
 public class VisitorInfo {
 
+    /**
+     * All visits that a visitor has made
+     */
     private Map<String, String> visits;
-
+    /**
+     * Identifying information for a visitor including name, address, and
+     * phone number
+     */
     private String[] info;
-
+    /**
+     * The unique visitor id
+     */
     private String visitorID;
 
+    /**
+     * Create visitor info given a line of info.
+     * @param info Correctly formatted info
+     */
     public VisitorInfo(String info) {
         // break whole string to each part of info
     }
 
+    /**
+     * Record a visit given a date.
+     * @param date The date at the start of the visit
+     */
     public void startVisit(String date) {
 
     }
 
+    /**
+     * End the last visit given a date.
+     * @param date The date at the end of the visit
+     */
     public void endVisit(String date) {
 
     }
 
+    /**
+     * Get the first name.
+     * @return First name
+     */
     public String getFirstName() {
         return null;
     }
 
+    /**
+     * Get the last name.
+     * @return Last name
+     */
     public String getLastName() {
         return null;
     }
 
+    /**
+     * Get the address.
+     * @return Address
+     */
     public String getAddress() {
         return null;
     }
 
+    /**
+     * Get the phone number.
+     * @return Phone number
+     */
     public String getPhoneNumber() {
         return null;
     }
