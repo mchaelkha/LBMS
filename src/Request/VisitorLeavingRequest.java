020302eb0ef23c4a9c3ed92ac6c2496a5c424ccb
lxg8800 <lxg8800@rit.edu>
2019-02-21 17:10:20 -0500

    Stubbed VisitorLeavingRequest

---
+++ src/Request/VisitorLeavingRequest.java
@@ -0,0 +1,13 @@
+package Request;
+
+public class VisitorLeavingRequest implements Request {
+    @Override
+    public void collectDependencies() {
+
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
+}
