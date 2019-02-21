306a201b7df67f314798b9ce68e61a14fe012937
Michael Kha <mxk5025@rit.edu>
2019-02-21 18:38:17 -0500

    Documenting checkout classes

--- src/Checkout/CheckoutDB.java
+++ src/Checkout/CheckoutDB.java
@@ -1,6 +1,10 @@
 package Checkout;
 
+import Book.BookDB;
+import Visitor.VisitorDB;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,23 +12,64 @@ import java.util.Map;
  */
 public class CheckoutDB {
 
-    private Map<String, Transaction> openLoans;
+    /**
+     * The open transaction loans of each visitor
+     */
+    private Map<String, List<Transaction>> openLoans;
+    /**
+     * The closed transaction loans of each visitor
+     */
+    private Map<String, List<Transaction>> closedLoans;
+    /**
+     * The visitor database
+     */
+    private VisitorDB visitorDB;
+    /**
+     * The book database
+     */
+    private BookDB bookDB;
 
-    private Map<String, Transaction> closedLoans;
-
-    public CheckoutDB() {
+    /**
+     * Create a new checkout database that is empty
+     */
+    public CheckoutDB(VisitorDB visitorDB, BookDB bookDB) {
+        this.visitorDB = visitorDB;
+        this.bookDB = bookDB;
         openLoans = new HashMap<>();
         closedLoans = new HashMap<>();
     }
 
+    /**
+     * Create a checkout transaction using a visitor ID and a book's ISBN.
+     * @param visitorID The visitor ID
+     * @param book The book's ISBN
+     * @return The new transaction
+     */
     public Transaction checkout(String visitorID, String book) {
         return null;
     }
 
-    public int calculateFine(String book) {
+    /**
+     * Calculate the fines accrued by a visitor.
+     * @param visitorID The visitor ID to check for
+     * @return The fine amount under a visitor ID
+     */
+    public int calculateFine(String visitorID) {
         return 0;
     }
 
+    /**
+     * Calculate the fines accrued by all visitors.
+     * @return The total fine amount
+     */
+    public int calculateTotalFines() {
+        return 0;
+    }
+
+    /**
+     * Return the book of the last transaction.
+     * @param visitorID The visitor ID to return books for
+     */
     public void returnBook(String visitorID) {
 
     }
--- src/Checkout/Transaction.java
+++ src/Checkout/Transaction.java
@@ -5,37 +5,70 @@ package Checkout;
  */
 public class Transaction {
 
+    /**
+     * The date that the checkout occurred
+     */
     public String checkoutDate;
-
+    /**
+     * The date that the book is due
+     */
     public String dueDate;
-
+    /**
+     * The date that the book is returned
+     */
     public String returnDate;
-
+    /**
+     * The book's ISBN
+     */
     public String book;
-
+    /**
+     * The fine amount of the transaction
+     */
     public int fineAmount;
 
-    public Transaction(String checkOutDate, String book) {
-        this.checkoutDate = checkOutDate;
+    /**
+     * Create a new transaction given a checkout date and the book.
+     * @param checkoutDate The checkout date
+     * @param book The book's ISBN
+     */
+    public Transaction(String checkoutDate, String book) {
+        this.checkoutDate = checkoutDate;
         this.book = book;
         // calculate due date
 
         fineAmount = 0;
     }
 
+    /**
+     * Return the book on the given date.
+     * @param date The date of return
+     */
     public void returnBook(String date) {
         returnDate = date;
         calculateFine();
     }
 
+    /**
+     * Calculate the number of late days after the due date.
+     * Days are rounded up.
+     * @return The number of late days after the due date
+     */
     public int calculateNumLateDays() {
         return 0;
     }
 
+    /**
+     * Get the fine amount.
+     * @return Fine amount
+     */
     public int getFineAmount() {
         return fineAmount;
     }
 
+    /**
+     * Calculate the fine amount from this transaction. This
+     * is determined by the number of late days.
+     */
     private void calculateFine() {
 
     }
