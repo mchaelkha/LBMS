af869426280e945bace157f661b116bd1a39e3f9
lxg8800 <lxg8800@rit.edu>
2019-02-21 17:11:23 -0500

    Stubbed SystemRestoreRequest

---
+++ src/Request/SystemRestoreRequest.java
@@ -0,0 +1,13 @@
+package Request;
+
+public class SystemRestoreRequest implements Request {
+    @Override
+    public void collectDependencies() {
+        
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
+}
