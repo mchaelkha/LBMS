d03ff8cc714988581ec1af77d3e252af89a33e65
lxg8800 <lxg8800@rit.edu>
2019-02-21 17:13:54 -0500

    Stubbed ShutDownRequest

---
+++ src/Request/ShutDownRequest.java
@@ -0,0 +1,13 @@
+package Request;
+
+public class ShutDownRequest implements Request {
+    @Override
+    public void collectDependencies() {
+        
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
+}
