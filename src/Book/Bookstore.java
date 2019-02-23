f4eb83a345bc5cdeedabe459ecfbd68324cfe31b
mxk5025 <michaelkha213@gmail.com>
2019-02-23 10:35:32 -0500

    Search feature for bookstore

--- src/Book/Bookstore.java
+++ src/Book/Bookstore.java
@@ -3,6 +3,7 @@ package Book;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Reads a properly formatted books file to initialize BookInfo objects to be
@@ -76,22 +77,99 @@ public class Bookstore {
         publishDate = publishing[1];
         pageCount = Integer.parseInt(publishing[2]);
         // Create book info
-        return new BookInfo(isbn, title, authors, publisher, publishDate, pageCount);
+        return new BookInfo(isbn, title, authors, publisher,
+                publishDate, pageCount);
     }
 
+    /**
+     * Search the books using a filter on all the available books for purchase.
+     *
+     * @param title Title search parameter
+     * @param authors Authors search parameter
+     * @param isbn ISBN search parameter
+     * @param publisher Publisher search parameter
+     * @param sort Sort the search by either title or publish-date
+     * @return The mapping of hits to a unique ID
+     */
     public Map<String, BookInfo> searchBooks(String title,
                                              List<String> authors,
-                                             String ibsn,
+                                             String isbn,
                                              String publisher, String sort) {
         Map<String, BookInfo> searchedBooks = new HashMap<>();
-
-        for (BookInfo info : books.values()) {
-            // need to match information
-
-            // TODO: use streams and filters?
+        // Filter out results into a list of search hits
+        List<BookInfo> hits = books.values().stream()
+                .filter(b -> matchingFilter(b, title, authors,
+                        isbn, publisher))
+                .collect(Collectors.toList());
+        // TODO: Sort the search hits now
+        switch (sort) {
+            case "title":
+                // Call title comparator
+                break;
+            case "publish-date":
+                // Call publish date comparator
+                break;
+            default:
+                // TODO: throw an invalid parameter exception
         }
+
+        // Map to a unique ID for the hits
+        int id = 0;
+        for (BookInfo info : hits) {
+            searchedBooks.put(String.valueOf(id), info);
+            id++;
+        }
+        // Set last search to this recent search
         lastSearch = searchedBooks;
         return searchedBooks;
+    }
+
+    /**
+     * Determines a matching between book info and its search parameters.
+     * Parameters are ignored if they equal '*'. Authors are ignored if the
+     * list is empty.
+     * @param book Book info to compare with
+     * @param title Title from search
+     * @param authors Authors from search
+     * @param isbn ISBN from search
+     * @param publisher Publisher from search
+     * @return If the book info has completely matched through each filter
+     */
+    private boolean matchingFilter(BookInfo book, String title,
+                                   List<String> authors,
+                                   String isbn, String publisher) {
+        String bookTitle = book.getTitle();
+        List<String> bookAuthors = book.getAuthors();
+        String bookIsbn = book.getIsbn();
+        String bookPublisher = book.getPublisher();
+        String ignore = "*";
+        if (!title.equals(ignore)) {
+            // Title may only contain a substring
+            if (!bookTitle.contains(title)) {
+                return false;
+            }
+        }
+        if (!authors.isEmpty()) {
+            for (String author : bookAuthors) {
+                // Authors must exactly match
+                if (!authors.contains(author)) {
+                    return false;
+                }
+            }
+        }
+        if (!isbn.equals(ignore)) {
+            // ISBN must exactly match
+            if (!bookIsbn.equals(isbn)) {
+                return false;
+            }
+        }
+        if (!publisher.equals(ignore)) {
+            // Publisher must exactly match
+            if (!bookPublisher.equals(publisher)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
