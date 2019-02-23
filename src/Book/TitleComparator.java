60bbccc94d92d331b570c438ad9b61ce828ce45f
mxk5025 <michaelkha213@gmail.com>
2019-02-23 12:24:21 -0500

    Documented and stubbed comparator

--- src/Book/TitleComparator.java
+++ src/Book/TitleComparator.java
@@ -2,9 +2,25 @@ package Book;
 
 import java.util.Comparator;
 
+/**
+ * Book info comparator for sorting by title in alphanumeric order.
+ *
+ * @author Michael Kha
+ */
 public class TitleComparator implements Comparator<BookInfo> {
+
+    /**
+     * Compare the books such that the titles are they are ordered
+     * alphanumerically.
+     * @param b1 First book
+     * @param b2 Second book
+     * @return The title ordering
+     */
     @Override
-    public int compare(BookInfo o1, BookInfo o2) {
+    public int compare(BookInfo b1, BookInfo b2) {
+        String firstTitle = b1.getTitle();
+        String secondTitle = b2.getTitle();
+
         return 0;
     }
 }
