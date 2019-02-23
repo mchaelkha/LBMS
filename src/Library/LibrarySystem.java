d5b8bf80df361f1482d05ab9f14a0b1c5fec5c80
Hersh Nagpal <hxn6993@rit.edu>
2019-02-23 02:19:08 -0500

    updated some documentation and renamed methods to be mroe descriptive.

--- src/Library/LibrarySystem.java
+++ src/Library/LibrarySystem.java
@@ -1,19 +1,27 @@
+/**
+ * 
+ * @author Hersh Nagpal
+ */
 package Library;
 
 import Request.Request;
 
 public class LibrarySystem {
+    private static int OPEN_HOUR = 9;
+    private static int CLOSE_HOUR = 12+9;
+
     private String openTime;
     private String closeTime;
     private String currentMonth;
     private DBManager manager;
+    private TimeKeeper timeKeeper;
 
     /**
      * Gives the status of the library
      * @return Whether the library is open.
      */
     public boolean isOpen(){
-        return false;
+        return timeKeeper.isLibraryOpen(OPEN_HOUR, CLOSE_HOUR);
     }
 
     /**
