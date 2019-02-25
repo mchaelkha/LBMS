c5fbd834da565ae53b32e7922b52c282d45af888
Hersh Nagpal <hxn6993@rit.edu>
2019-02-24 20:47:38 -0500

    Attempted to make test classes

---
+++ test/TimeKeeperTest.java
@@ -0,0 +1,14 @@
+import Library.TimeKeeper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeKeeperTest {
+    @test
+    testUpdateTime() {
+        TimeKeeper tK = new TimeKeeper();
+        
+    }
+}
