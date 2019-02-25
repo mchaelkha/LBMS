d25343d6f139c42bbdba4a16aeacbd6483e233d0
mxk5025 <michaelkha213@gmail.com>
2019-02-24 22:29:54 -0500

    Started request parser

--- src/Request/RequestParser.java
+++ src/Request/RequestParser.java
@@ -1,7 +1,83 @@
 package Request;
 
+/**
+ * Work in progress
+ * Parse strings into requests to be executed. Parsing checks for errors
+ * in invalid format.
+ *
+ * @author Michael Kha
+ */
 public class RequestParser{
-    public Request determineRequest(String request){
+
+    private static final String DELIMITER = ",";
+    private static final String TERMINATOR = ";";
+
+    private String partial;
+
+    public RequestParser() {
+        partial = "";
+    }
+
+    public Request determineRequest(String request) {
+        // Check if partial request first
+        if (!request.endsWith(TERMINATOR) || !partial.isEmpty()) {
+            partial += request;
+            if (request.endsWith(TERMINATOR)) {
+                request = partial;
+                partial = "";
+                return helpCreateRequest(request);
+            }
+            // return partial request
+            return null;
+        }
+        else {
+            return helpCreateRequest(request);
+        }
+    }
+
+    private Request helpCreateRequest(String request) {
+        // Break request into a command and its parameters
+        String[] parts = request.split(DELIMITER, 2);
+        if (parts.length != 2) {
+            // TODO: throw exception
+        }
+        // Determine which request
+        return createRequest(parts[0], parts[1]);
+    }
+
+    private Request createRequest(String command, String params) {
+        switch (command) {
+            case "partial-request":
+                break;
+            case "register":
+                break;
+            case "arrive":
+                break;
+            case "depart":
+                break;
+            case "info":
+                break;
+            case "borrow":
+                break;
+            case "borrowed":
+                break;
+            case "return":
+                break;
+            case "pay":
+                break;
+            case "search":
+                break;
+            case "buy":
+                break;
+            case "advance":
+                break;
+            case "datetime":
+                break;
+            case "report":
+                break;
+            default:
+                break;
+        }
         return null;
     }
 }
