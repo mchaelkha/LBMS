4d737ffa55bbba35c0d76b2ca191efa4f97ea796
Jack Li <jl1429@rit.edu>
2019-02-26 16:00:46 -0500

    method shell for createRequest().

--- src/Controller/RequestParser.java
+++ src/Controller/RequestParser.java
@@ -1,10 +1,8 @@
 package Controller;
 
-import Request.RequestUtil;
+import Request.*;
 import Book.BookDB;
 import Checkout.CheckoutDB;
-import Request.Request;
-import Request.Partial;
 import Visitor.VisitorDB;
 
 /**
@@ -73,36 +71,60 @@ public class RequestParser implements RequestUtil {
     }
 
     /**
-     *
-     * @param command
-     * @param params
-     * @return
+     * Creates, executes, and returns the request, given a command, and parameters.
+     * @param command The request to execute
+     * @param params The parameters for the command.
+     * @return The request that was executed.
      */
     private Request createRequest(String command, String params) {
         switch (command) {
             case REGISTER_REQUEST:
+                RegisterVisitor reg = new RegisterVisitor();
+                reg.execute();
                 break;
             case ARRIVE_REQUEST:
+                BeginVisit beg = new BeginVisit();
+                beg.execute();
                 break;
             case DEPART_REQUEST:
+                EndVisit end = new EndVisit();
+                end.execute();
                 break;
             case INFO_REQUEST:
+                LibraryStatisticsReport libstat = new LibraryStatisticsReport();
+                libstat.execute();
                 break;
             case BORROW_REQUEST:
+                BorrowBook borrow = new BorrowBook();
+                borrow.execute();
                 break;
             case BORROWED_REQUEST:
+                FindBorrowedBooks findb = new FindBorrowedBooks();
+                findb.execute();
                 break;
             case RETURN_REQUEST:
+                ReturnBook rtn = new ReturnBook();
+                rtn.execute();
                 break;
             case PAY_REQUEST:
+                PayFine pay = new PayFine();
+                pay.execute();
                 break;
             case SEARCH_REQUEST:
+                LibraryBookSearch libsearch = new LibraryBookSearch();
+                libsearch.execute();
                 break;
             case BUY_REQUEST:
+                BookPurchase purchase = new BookPurchase();
+                purchase.execute();
                 break;
             case ADVANCE_REQUEST:
+                AdvanceTime adv = new AdvanceTime();
+                adv.execute();
                 break;
             case DATE_TIME_REQUEST:
+                CurrentDateTime date = new CurrentDateTime();
+                date.execute();
                 break;
             case REPORT_REQUEST:
                 break;
