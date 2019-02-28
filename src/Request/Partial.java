ef046ff5b92c60dc538f9081e2fa424a9c10cfb5
mxk5025 <michaelkha213@gmail.com>
2019-02-27 22:22:24 -0500

    A partial request! It is very interesting...

--- src/Request/Partial.java
+++ src/Request/Partial.java
@@ -1,14 +1,28 @@
 package Request;
 
+/**
+ * Partial request to represent any request that is non-terminated.
+ *
+ * @author Michael Kha
+ */
 public class Partial implements Request {
+
+    /**
+     * @deprecated  Not used for partial requests
+     */
+    @Deprecated
     @Override
     public String checkParams() {
         return "";
     }
 
+    /**
+     * Execute the partial request by displaying a message.
+     * @return
+     */
     @Override
     public String execute() {
-        return null;
+        return PARTIAL_REQUEST;
     }
 
 }
