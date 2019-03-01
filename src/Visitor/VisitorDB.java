dcbac682704ad5ebea57e672d565860dd8e4a36c
lxg8800 <lxg8800@rit.edu>
2019-03-01 11:25:34 -0500

    Implemented endVisit() in VisitorDB

--- src/Visitor/VisitorDB.java
+++ src/Visitor/VisitorDB.java
@@ -6,6 +6,7 @@ import Library.TimeKeeper;
 import Request.RequestUtil;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +36,11 @@ public class VisitorDB implements RequestUtil, Serializable{
     private final int INITIAL_VISITOR_ID = 1000000000;
 
     /**
+     * Updated when Visitor begins a visit. Used to calculate visit duration
+     */
+    private LocalDateTime startDayTime;
+
+    /**
      * Create a new visitor database that is empty.
      */
     public VisitorDB() {
@@ -59,7 +65,7 @@ public class VisitorDB implements RequestUtil, Serializable{
         for (String currentKey : registeredVisitors.keySet()) {
             //Duplicate visitorInfo found
             if (registeredVisitors.get(currentKey).equals(newVisitorInfo)) {
-                return ARRIVE_REQUEST + DELIMITER + DUPLICATE;
+                return ARRIVE_REQUEST + DELIMITER + DUPLICATE + TERMINATOR;
             }
         }
         //No duplicate was found. Register new visitor.
@@ -70,7 +76,7 @@ public class VisitorDB implements RequestUtil, Serializable{
         TimeKeeper timeKeeper = TimeKeeper.getInstance();
         String registeredDate = timeKeeper.readDate();
         return REGISTER_REQUEST+DELIMITER+newVisitorIDString
-                +DELIMITER+registeredDate;
+                +DELIMITER+registeredDate+TERMINATOR;
     }
 
     /**
@@ -81,12 +87,12 @@ public class VisitorDB implements RequestUtil, Serializable{
         //Check if visitor with id already exists in currentVisitors
         if (currentVisitors.containsKey(visitorID)) {
             //Response = "arrive,duplicate";
-            return ARRIVE_REQUEST+DELIMITER+DUPLICATE;
+            return ARRIVE_REQUEST+DELIMITER+DUPLICATE+TERMINATOR;
         }
         //Check if visitor has not registered yet
         else if (!registeredVisitors.containsKey(visitorID)){
             //Response = "arrive,invalid-id";
-            return ARRIVE_REQUEST+DELIMITER+INVALID_ID;
+            return ARRIVE_REQUEST+DELIMITER+INVALID_ID+TERMINATOR;
         }
         //Add visitor to currentVisitors and update its state
         else{
@@ -95,9 +101,13 @@ public class VisitorDB implements RequestUtil, Serializable{
             TimeKeeper timeKeeper = TimeKeeper.getInstance();
             String visitDate = timeKeeper.readDate();
             String visitTime = timeKeeper.readTime();
+
+            //Get LocalDateTime for the startVisit time
+            LocalDateTime startVisitDateTime = timeKeeper.getClock();
+
             //Response = "arrive,visitorID,visitDate,visitStartTime"
             return ARRIVE_REQUEST+DELIMITER+visitorID+DELIMITER
-                    +visitDate+DELIMITER+visitTime;
+                    +visitDate+DELIMITER+visitTime+TERMINATOR;
             //TODO change visitorInfo state to inLabrary if using state pattern
         }
     }
@@ -106,10 +116,25 @@ public class VisitorDB implements RequestUtil, Serializable{
      * End a visit that is in progress given the visitor id.
      * @param visitorID The visitor id to end a visit for
      */
-    public void endVisit(String visitorID) {
+    public String endVisit(String visitorID) {
+        //Check if visitorID is not currently in current visitors
+        if(currentVisitors.containsKey(visitorID)){
+            //Response = "arrive,invalid-id;"
+            return DEPART_REQUEST+DELIMITER+INVALID_ID+TERMINATOR;
+        }
+
         //Remove visitor from currentVisitors
         currentVisitors.remove(visitorID);
-        //TODO change visitor state to not in library if using state pattern
+
+        //Response = "depart,visitorID,visitEndTime,visitDuration"
+        TimeKeeper timeKeeper = TimeKeeper.getInstance();
+        String visitEndTime = timeKeeper.readTime();
+
+        LocalDateTime endVisitDateTime = timeKeeper.getClock();
+        String visitDuration = timeKeeper.calculateDuration(startDayTime, endVisitDateTime);
+
+        return DEPART_REQUEST+DELIMITER+visitorID+DELIMITER+visitEndTime+
+                DELIMITER+visitDuration;
     }
 
     /**
@@ -124,7 +149,7 @@ public class VisitorDB implements RequestUtil, Serializable{
      * Finds a visitor, determines whether or not they can
      * checkout a book, and then adds a book if they can.
      */
-    public boolean checkOutBook(String visitorID, Transaction transaction) {
+    public boolean checkoutBook(String visitorID, Transaction transaction) {
 
         //Check if there is a current visitor with visitorID
         if (!currentVisitors.containsKey(visitorID)) {
