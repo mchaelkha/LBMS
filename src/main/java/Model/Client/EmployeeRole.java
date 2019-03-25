eb992a5dfd7d162aded17af48de0a9efa2d63d55
Hersh Nagpal <hxn6993@rit.edu>
2019-03-25 10:55:30 -0400

    Stubbed command methods in Role interface.

--- src/main/java/Model/Client/EmployeeRole.java
+++ src/main/java/Model/Client/EmployeeRole.java
@@ -1,4 +1,84 @@
 package Model.Client;
 
 public class EmployeeRole implements Role {
+
+    @Override
+    public String advanceTime() {
+        return null;
+    }
+
+    @Override
+    public String beginVisit() {
+        return null;
+    }
+
+    @Override
+    public String purchaseBook() {
+        return null;
+    }
+
+    @Override
+    public String searchStore() {
+        return null;
+    }
+
+    @Override
+    public String borrowBook() {
+        return null;
+    }
+
+    @Override
+    public String createAccount() {
+        return null;
+    }
+
+    @Override
+    public String currentDateTime() {
+        return null;
+    }
+
+    @Override
+    public String endVisit() {
+        return null;
+    }
+
+    @Override
+    public String findBorrowedBooks() {
+        return null;
+    }
+
+    @Override
+    public String libraryBookSearch() {
+        return null;
+    }
+
+    @Override
+    public String libraryStatisticsReport() {
+        return null;
+    }
+
+    @Override
+    public String login() {
+        return null;
+    }
+
+    @Override
+    public String logout() {
+        return null;
+    }
+
+    @Override
+    public String payFine() {
+        return null;
+    }
+
+    @Override
+    public String registerVisitor() {
+        return null;
+    }
+
+    @Override
+    public String returnBook() {
+        return null;
+    }
 }
--- src/main/java/Model/Client/Role.java
+++ src/main/java/Model/Client/Role.java
@@ -1,5 +1,7 @@
 package Model.Client;
 
+import Controller.Request.RegisterVisitor;
+
 /**
  * Implementation of the state pattern for the purpose of allowing different
  * behavior between visitor and employee requests.
@@ -7,5 +9,20 @@ package Model.Client;
  * @author Michael Kha
  */
 public interface Role {
-    //TODO: add role specific requests
+    public String advanceTime();
+    public String beginVisit();
+    public String purchaseBook();
+    public String searchStore();
+    public String borrowBook();
+    public String createAccount();
+    public String currentDateTime();
+    public String endVisit();
+    public String findBorrowedBooks();
+    public String libraryBookSearch();
+    public String libraryStatisticsReport();
+    public String login();
+    public String logout();
+    public String payFine();
+    public String registerVisitor();
+    public String returnBook();
 }
