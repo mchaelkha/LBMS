1f11379dc8d573f9abdfa1f8074a3e2b62ad5532
lxg8800 <lxg8800@rit.edu>
2019-03-28 15:54:17 -0400

    Renamed RequestAbstract

--- src/main/java/Controller/Request/AccessibleRequests.java
+++ src/main/java/Controller/Request/AccessibleRequests.java
@@ -3,11 +3,11 @@ package Controller.Request;
 /**
  * Allows requests to know if they are undoable or employee only
  */
-public abstract class RequestAbstract implements Request {
+public abstract class AccessibleRequests implements Request {
     private boolean undoable;
     private boolean employeeOnly;
 
-    public RequestAbstract(boolean undoable, boolean employeeOnly) {
+    public AccessibleRequests(boolean undoable, boolean employeeOnly) {
         this.undoable = undoable;
         this.employeeOnly = employeeOnly;
     }
